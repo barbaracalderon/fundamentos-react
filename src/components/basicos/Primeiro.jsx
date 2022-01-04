6ae5d4c341e6cbf49f0c6f254f3c1a810c886ab2
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-04 16:59:32 -0300

    Created #01 component

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Primeiro() {
+    const msg = 'Bem-vindo(a) ao mundo React.'
+    return (
+        <div>
+            <h1>#01 Componente</h1>
+            <p> { msg } </p>
+        </div>
+    )
+}
