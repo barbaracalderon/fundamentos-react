ea410f7e73cafbe9826c27b3228e8b314934aa3a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-04 18:47:47 -0300

    Updated component #03 to empty tags

--- src/components/basicos/Fragmento.jsx
+++ src/components/basicos/Fragmento.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Fragmento(props) {
     return (
-        <div>
+        <>
             <h2>#03 - Componente Fragmento</h2>
             <p>Cuidado com esse erro.</p>
-        </div>
+        </>
     )
 }
