bc6f96628ad9d97c918f3eb903029d40bf07c6fc
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-04 17:06:49 -0300

    Created segundo componente

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const el = document.getElementById('root')
 
 ReactDOM.render(
   <div>
     <Primeiro/>
+    <ComParametro
+      titulo="#02 Componente"
+      subtitulo="Primeiros passos."
+      msg="Estamos usando uma função com parâmetro."
+    />
   </div>,
    el
 )
