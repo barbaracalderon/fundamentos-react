af8703f8be73377f06e1c7d9e2c5fa43de073869
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-05 18:38:40 -0300

    Updated component #11 with 'useState' (React Hooks)

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 export default props => {
+    const gerarIdade = () => parseInt(Math.random() * (40)) + 40
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>
                 <p>Filho:</p>
                 <button onClick={e => 
-                        props.quandoClicar('João', 53, true)
+                        props.quandoClicar('João', gerarIdade(), gerarNerd())
                 }>Fornecer informações</button>
             </div>
-
         </div>
     )
 }
