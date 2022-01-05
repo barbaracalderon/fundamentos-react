799a1d508a535a2150b3835678100c2b121cfd92
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-05 02:03:41 -0300

    Updated == to ===

--- src/components/repeticao/TabelaProdutos.jsx
+++ src/components/repeticao/TabelaProdutos.jsx
@@ -5,7 +5,7 @@ import '../layout/ListaProdutos.css'
 export default props => {
     const listaDeProdutos = produtos.map((produto, i) => {
         return (
-                <tr key={produto.id} className={i % 2 == 0 ? 'Par' : 'Impar'}>
+                <tr key={produto.id} className={i % 2 === 0 ? 'Par' : 'Impar'}>
                     <td> { produto.id } </td>
                     <td> { produto.nome } </td>
                     <td> { produto.preco } </td>
